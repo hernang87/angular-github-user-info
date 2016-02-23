cd24413dca8aeb68960d6776433e7bdb1b47a976
Hernan Garchtrom <hernan.garchtrom@rga.com>
2016-02-23 16:01:19 -0300

    Added auth

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,12 +1,24 @@
 var app = angular.module('githubUserInfoApp', []);
 
 app.controller('userSearchCtrl', ['$scope', function($scope) {
-	$scope.username = '';		
+	$scope.username = '';	
+	$scope.users = [];	
+
 	$scope.getUsers = function() {		
-		if($scope.username.length > 3) {
-			$.get('http://api.github.com/search/users?q=' + $scope.username, function(data) {
+		if($scope.username.length > 2) {
+			$.get('http://api.github.com/search/users?client_id=c2df7d0a60a47904f605&client_secret=f492970143a019eb640a29f3dc9df2b185e22f37&q=' + $scope.username, function(data) {
 				if(data.items) {
-					$scope.users = data.items;
+					data.items.forEach(function(elem) {
+						var obj = {
+							avatar_url: elem.avatar_url,
+							login: elem.login,
+							followers: []
+						};
+
+						$.get('http://api.github.com/users/' + elem.login + '/followers?client_id=c2df7d0a60a47904f605&client_secret=f492970143a019eb640a29f3dc9df2b185e22f37', function(followers) {
+							console.log(followers);
+						});
+					});
 				}
 			});			
 		}
