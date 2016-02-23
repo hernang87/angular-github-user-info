62efb401b2f38e5f8f2b61602fc94873acd3afc2
Hernan Garchtrom <hernan.garchtrom@rga.com>
2016-02-23 16:06:11 -0300

    Followers

--- dev/js/app.js
+++ dev/js/app.js
@@ -16,8 +16,12 @@ app.controller('userSearchCtrl', ['$scope', function($scope) {
 						};
 
 						$.get('http://api.github.com/users/' + elem.login + '/followers?client_id=c2df7d0a60a47904f605&client_secret=f492970143a019eb640a29f3dc9df2b185e22f37', function(followers) {
-							console.log(followers);
+							followers.forEach(function(follower) {
+								obj.followers.push(follower.login);
+							});
 						});
+
+						$scope.users.push(obj);
 					});
 				}
 			});			
